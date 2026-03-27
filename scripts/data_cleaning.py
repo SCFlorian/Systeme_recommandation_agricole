d69f4ec1bf2b637bc92f62f2d852fd1dbe130339
Florian Schorer <schorer.florian@me.com>
2026-03-27 16:19:32 +0100

    Mise à jour du script de nettoyage

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -30,8 +30,7 @@ def cleaning_crop_yield():
 # Fonction d'harmonisation du nom des pays
 
 def harmonization_countries():
-    """ Fonction pour harmoniser les types de culture entre le
-    fichier principal et le fichier synthétique"""
+    """ Fonction pour harmoniser le nom des pays par fichier"""
     try :
         data_pesticides = pd.read_csv(csv_pesticides)
         data_rainfall = pd.read_csv(csv_rainfall)
@@ -67,3 +66,100 @@ def harmonization_countries():
     logging.info("Nom standardisé des pays par fichier")
 
     return data_yield_map, data_rainfall_map, data_pesticides_map, data_temp_map
+
+def cleaning_files(data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map):
+    """"Fonction de nettoyage de chaque fichier avant fusion dans un fichier consolidé"""
+    # Fichier yield
+    # Restriction à la période commune 1990-2016
+    data_yield_map = data_yield_map[data_yield_map["Year"].between(1990, 2016)].copy()
+    # On garde les colonnes utiles
+    # On enlève notamment Domain Code / Area Code / Element Code / Item Code / Unit
+    data_yield_map = data_yield_map[["Area", "Item", "Year", "Value"]].copy()
+
+    # On renomme correctement le nom de la variable cible
+    data_yield_map = data_yield_map.rename(columns={"Area":"area",
+                                                    "Item":"item",
+                                                    "Year":"year",
+                                                    "Value":"yield"}).copy()
+    # On supprime les derniers doublons
+    data_yield_map = data_yield_map.drop_duplicates(subset=["area", "item", "year"]).copy()
+    logging.info(
+        "Doublons sur (Area, Item, Year) dans yield : %s",
+        data_yield_map.duplicated(["area", "item", "year"]).sum()
+    )
+    logging.info(
+        "Valeurs manquantes dans yield : %s",
+        data_yield_map.isnull().mean()
+)
+
+    # Fichier pesticides
+    # Restriction à la période commune 1990-2016
+    data_pesticides_map = data_pesticides_map[data_pesticides_map["Year"].between(1990, 2016)].copy()
+    # On garde les colonnes utiles
+    # On enlève notamment Domain Code / Area Code / Element Code / Item Code / Unit
+    data_pesticides_map = data_pesticides_map[["Area", "Year", "Value"]].copy()
+    # On renomme correctement le nom de la variable cible
+    data_pesticides_map = data_pesticides_map.rename(columns={"Area":"area",
+                                                            "Year":"year",
+                                                            "Value":"pesticides_tonnes"}).copy()
+    # Vérification des doublons métier
+   
+    logging.info(
+    "Doublons sur (Area, Year) dans pesticides : %s",
+    data_pesticides_map.duplicated(["area", "year"]).sum()
+    )
+    logging.info(
+        "Valeurs manquantes dans pesticides : %s",
+        data_pesticides_map.isnull().mean()
+    )
+
+    # Fichier rainfall
+    # Restriction à la période commune 1990-2016
+    data_rainfall_map = data_rainfall_map[data_rainfall_map["Year"].between(1990, 2016)].copy()
+    # On renomme correctement 2 variables
+    data_rainfall_map = data_rainfall_map.rename(columns={" Area": "area",
+                                                        "Year":"year",
+                                                        "average_rain_fall_mm_per_year": "rainfall_mm"}).copy()
+    # Conversion numérique en forçant malgrés les NaN
+    data_rainfall_map["rainfall_mm"] = pd.to_numeric(data_rainfall_map["rainfall_mm"],errors="coerce")
+    # Vérification des doublons métier
+    logging.info(
+        "Doublons sur (Area, Year) dans rainfall : %s",
+        data_rainfall_map.duplicated(["area", "year"]).sum()
+    )
+    logging.info(
+        "Valeurs manquantes dans rainfall : %s",
+        data_rainfall_map.isnull().mean()
+    )
+
+    # Fichier temp
+    # Restriction à la période commune 1990-2016
+    data_temp_map = data_temp_map[data_temp_map["year"].between(1990, 2016)].copy()
+    # On renomme correctement 2 variables
+    data_temp_map = data_temp_map.rename(columns={"country": "area"}).copy()
+    # Agrégation propre au couple Area-Year
+    # Imputation par la moyenne pour les années manquantes par pays / Imputation logique ici avant imputation globale
+    data_temp_map = (
+        data_temp_map
+        .groupby(["area", "year"], as_index=False)["avg_temp"]
+        .median()
+    ).copy()
+    # Vérification des doublons métier
+    logging.info(
+        "Doublons sur (Area, Year) dans temp : %s",
+        data_temp_map.duplicated(["area", "year"]).sum()
+    )
+    logging.info(
+        "Valeurs manquantes dans temp : %s",
+        data_temp_map.isnull().mean()
+    )
+    data_temp_map.head()
+
+    return data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map
+
+data_yield_map, data_rainfall_map, data_pesticides_map, data_temp_map = harmonization_countries()
+data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map = cleaning_files(data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map)
+logging.info(data_yield_map.head())
+logging.info(data_pesticides_map.head())
+logging.info(data_rainfall_map.head())
+logging.info(data_temp_map.head())
