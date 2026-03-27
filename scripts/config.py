a86bf3404e8171ab6dc1d4bdb86a84943febdcb6
Florian Schorer <schorer.florian@me.com>
2026-03-27 15:18:01 +0100

    Ajout de fonction de nettoyage

--- scripts/config.py
+++ scripts/config.py
@@ -1,2 +1,13 @@
+# ====================================================
 # Configuration du système de recommandation agricole
-csv_file_model = "data/raw/crop_yield.csv"
+# ====================================================
+
+# Chargement des fichiers csv
+csv_file_crop_yield = "data/raw/crop_yield.csv"
+csv_yield_conso = "data/processed/yield_df_final.csv"
+csv_file_crop_yield_clean = "data/raw/crop_yield.csv"
+csv_mapping_countries = "data/processed/harmonization_data/country_standardization_mapping.csv"
+csv_pesticides = "data/raw/pesticides.csv"
+csv_rainfall = "data/raw/rainfall.csv"
+csv_temp = "data/raw/temp.csv"
+csv_yield = "data/raw/yield.csv"
--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,6 +1,6 @@
-# ======================================
-# NETTOYAGE DU FICHIER "crop_yield.csv"
-# ======================================
+# =======================
+# Nettoyage des fichiers
+# =======================
 
 # Imports
 import pandas as pd
@@ -9,32 +9,61 @@ import logging
 # Chemin pour accèder aux éléments du projet
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Imports du projet
-from scripts.config import csv_file_model
+from scripts.config import (csv_file_crop_yield, csv_yield_conso, csv_file_crop_yield_clean, csv_mapping_countries,
+                            csv_pesticides, csv_rainfall, csv_temp, csv_yield)
 # Configuration du logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Fonction de nettoyage du fichier principal
-def cleaning_df():
-    """On prépare notre dataframe principal pour la modélisation"""
-    df = pd.read_csv(csv_file_model)
-    logging.info(f"\nNombre de lignes et de colonnes sans modifs : {df.shape}\n")
-    # On enlève les valeurs extrêmes (les rendements négatifs non explicables)
-    df_clean = df[df["Yield_tons_per_hectare"]>=0]
-    return df_clean
-# Affichage du nouveau fichier
-df_clean = cleaning_df()
-logging.info(df_clean.head())
-logging.info(f"\nNombre de lignes et de colonne aprés suppression : {df_clean.shape}")
+# Fonction de nettoyage du fichier crop_yield
+def cleaning_crop_yield():
+    """Sur le fichier crop_yield, le problème principal est les valeurs extrêmes.
+    On a identifié 231 valeurs négatives sur la variable cible"""
+    # On charge le fichier
+    df = pd.read_csv(csv_file_crop_yield)
+    logging.info(f"Nombre de lignes et de colonnes sans modifs : {df.shape}\n")
+    # On ne retient pas les éléments en dessous de 0
+    df = df[df['Yield_tons_per_hectare']>=0]
+    logging.info(f"Nombre de lignes et de colonnes sans modifs : {df.shape}\n")
 
-def preparation_modelisation(df_clean):
-    """ Encodage des variables non numériques"""
-    df = df_clean
-    # Encodage des variables qualitatives avec la technique One Hot
-    df_encoded = pd.get_dummies(df, columns=['Region','Soil_Type','Crop','Weather_Condition'], dtype=int)
-    # True et False des variables Fertilizer_Used et Irrigation_Used en catégorie 0 et 1
-    df_encoded['Fertilizer_Used'] = df_encoded['Fertilizer_Used'].astype(int)
-    df_encoded['Irrigation_Used'] = df_encoded['Irrigation_Used'].astype(int)
-    logging.info(f"\nNombre de lignes et de colonnes avec modifs : {df_encoded.shape}\n")
-    return df_encoded
+    return df
 
-df_encoded = preparation_modelisation(df_clean)
+# Fonction d'harmonisation du nom des pays
+
+def harmonization_countries():
+    """ Fonction pour harmoniser les types de culture entre le
+    fichier principal et le fichier synthétique"""
+    try :
+        data_pesticides = pd.read_csv(csv_pesticides)
+        data_rainfall = pd.read_csv(csv_rainfall)
+        data_temp = pd.read_csv(csv_temp)
+        data_yield = pd.read_csv(csv_yield)
+        mapping_countries = pd.read_csv(csv_mapping_countries)
+
+    except Exception as e:
+        logging.error(f"Erreur critique lors du chargement des fichiers : {e}")
+        raise e
+
+    # Chargement de la liste des pays pour harmonisation dans data/processed
+    mapping_df = mapping_countries
+    # On met les informations dans un dictionnaire afin de pouvoir les mapper
+    mapping_dict = dict(zip(mapping_df['Original Name'], mapping_df['Standard Name']))
+    # Identification d'une erreur supplémentaire
+    mapping_dict.update({"Bosnia And Herzegovina": "Bosnia and Herzegovina"})
+
+    # Mapping des fichiers
+    # Fichier yield
+    data_yield_map = data_yield
+    data_yield_map['Area'] = data_yield_map['Area'].map(mapping_dict).fillna(data_yield_map['Area'])
+    # Fichier rainfall
+    data_rainfall_map = data_rainfall
+    data_rainfall_map[' Area'] = data_rainfall_map[' Area'].map(mapping_dict).fillna(data_rainfall_map[' Area'])
+    # Fichier pesticide
+    data_pesticides_map = data_pesticides
+    data_pesticides_map['Area'] = data_pesticides_map['Area'].map(mapping_dict).fillna(data_pesticides_map['Area'])
+    # Fichier temp
+    data_temp_map = data_temp
+    data_temp_map['country'] = data_temp_map['country'].map(mapping_dict).fillna(data_temp_map['country'])
+
+    logging.info("Nom standardisé des pays par fichier")
+
+    return data_yield_map, data_rainfall_map, data_pesticides_map, data_temp_map
