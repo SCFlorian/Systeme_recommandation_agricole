d324c978c036b73462088f761321eaa4f14e113b
Florian Schorer <schorer.florian@me.com>
2026-03-29 15:35:11 +0200

    Mise à jour des notebooks avec l'ajout des scripts de nettoyage

--- scripts/config.py
+++ scripts/config.py
@@ -5,9 +5,10 @@
 # Chargement des fichiers csv
 csv_file_crop_yield = "data/raw/crop_yield.csv"
 csv_yield_conso = "data/processed/yield_df_final.csv"
-csv_file_crop_yield_clean = "data/raw/crop_yield.csv"
+csv_file_crop_yield_clean = "data/processed/crop_yield.csv"
 csv_mapping_countries = "data/processed/harmonization_data/country_standardization_mapping.csv"
 csv_pesticides = "data/raw/pesticides.csv"
 csv_rainfall = "data/raw/rainfall.csv"
 csv_temp = "data/raw/temp.csv"
 csv_yield = "data/raw/yield.csv"
+csv_yield_enriched = "data/processed/yield_df_enriched.csv"
--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,6 +1,6 @@
-# =======================
-# Nettoyage des fichiers
-# =======================
+# ========================================================
+# Nettoyage des fichiers à partir des fichiers consolidés
+# ========================================================
 
 # Imports
 import pandas as pd
@@ -9,157 +9,93 @@ import logging
 # Chemin pour accèder aux éléments du projet
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Imports du projet
-from scripts.config import (csv_file_crop_yield, csv_yield_conso, csv_file_crop_yield_clean, csv_mapping_countries,
-                            csv_pesticides, csv_rainfall, csv_temp, csv_yield)
+from scripts.config import (csv_yield_conso, csv_yield_enriched)
 # Configuration du logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Fonction de nettoyage du fichier crop_yield
-def cleaning_crop_yield():
-    """Sur le fichier crop_yield, le problème principal est les valeurs extrêmes.
-    On a identifié 231 valeurs négatives sur la variable cible"""
+# Fichier yield_df_conso
+
+def preparation_yield_df():
+    """L'objectif ici est de partir du fichier consolidé pour préparer le fichier à la modélisation.
+    On va renommer les variables et ensuite on va procéder au feature engineering"""
     # On charge le fichier
-    df = pd.read_csv(csv_file_crop_yield)
+    try :
+        df = pd.read_csv(csv_yield_conso, index_col=0)
+    except Exception as e:
+        logging.error(f"Erreur critique lors du chargement du fichier : {e}")
+        raise e
+    # On remplace les noms pour être plus cohérent
     logging.info(f"Nombre de lignes et de colonnes sans modifs : {df.shape}\n")
-    # On ne retient pas les éléments en dessous de 0
-    df = df[df['Yield_tons_per_hectare']>=0]
-    logging.info(f"Nombre de lignes et de colonnes sans modifs : {df.shape}\n")
+    df['item'] = df['item'].replace({
+        'Rice, paddy': 'Rice',
+        'Soybeans': 'Soybean',
+        'Maize': 'Maize',
+        'Wheat': 'Wheat',
+        'Barley': 'Barley'
+        })
+    logging.info(df['item'].unique())
+    # Ajout des nouvelles variables
+    # Progrès technique
+    df['tech_trend'] = df['year'] - df['year'].min()
+    # Écart-type de la température par pays (mesure de l'instabilité)
+    df['climate_instability'] = df.groupby('area')['avg_temp'].transform('std')
+    # Le relative tech-age
+    avg_pest_country = df.groupby('area')['pesticides_tonnes'].transform('mean')
+    df['relative_tech_intensity'] = df['pesticides_tonnes'] / (avg_pest_country + 1)
+    # Suppression de l'année et de l'area
+    df = df.drop(columns={"year","area"}).copy()
+    logging.info(f"Nombre de lignes et de colonnes après nouvelles variables et suppression de 2 colonnes : {df.shape}\n")
+    # On détermine quels sont nos varibales catégorielles et nos variables quantitatives
+    num_cols = df.select_dtypes(include='number').columns
+    cat_cols = df.select_dtypes(include='str').columns
+    logging.info(num_cols)
+    logging.info(cat_cols)
+    # Encodage One Hot des variables catégorielles
+    df = pd.get_dummies(df, columns=cat_cols, dtype=float).copy()
+    logging.info(f"Nombre de lignes et de colonnes après feature engineering {df.shape}")
+    # Nettoyage du nom des variables pour uniformiser
+    df.columns = df.columns.str.lower().str.replace(" ", "_")
+    logging.info(df.head())
+    logging.info(df.columns)
+    return df
+
+# Fichier crop_yield
+
+def preparation_yield_enriched():
+    """L'objectif ici est de partir du fichier enrichi pour préparer le fichier à la modélisation.
+    On va renommer les variables et ensuite on va procéder au feature engineering"""
+    # On charge le fichier
+    try :
+        df = pd.read_csv(csv_yield_enriched, index_col=0)
+    except Exception as e:
+        logging.error(f"Erreur critique lors du chargement du fichier : {e}")
+        raise e
+    logging.info(df.columns)
+    # Ajout des nouvelles variables
+    # Progrès technique
+    df['tech_trend'] = df['year'] - df['year'].min()
+    # Ratio climatique - indice d'irrigation critique
+    df['irrigation_impact'] = df['Irrigation_Used'].astype(int) / (df['rainfall_mm'] + 1)
+    # Écart-type de la température par pays (mesure de l'instabilité)
+    df['climate_instability'] = df.groupby('area')['avg_temp'].transform('std')
+    # Le relative tech-age
+    avg_pest_country = df.groupby('area')['pesticides_tonnes'].transform('mean')
+    df['relative_tech_intensity'] = df['pesticides_tonnes'] / (avg_pest_country + 1)
+    # Suppression de l'année et de l'area
+    df = df.drop(columns={"year","area"}).copy()
+    logging.info(f"Nombre de lignes et de colonnes après nouvelles variables et suppression de 2 colonnes : {df.shape}\n")
+    # On détermine quels sont nos varibales catégorielles et nos variables quantitatives
+    num_cols = df.select_dtypes(include='number').columns
+    cat_cols = df.select_dtypes(include=['str','bool']).columns
+    logging.info(num_cols)
+    logging.info(cat_cols)
+    # Encodage One Hot des variables catégorielles
+    df = pd.get_dummies(df, columns=cat_cols, dtype=float).copy()
+    logging.info(f"Nombre de lignes et de colonnes après feature engineering {df.shape}")
+    # Nettoyage du nom des variables pour uniformiser
+    df.columns = df.columns.str.lower().str.replace(" ", "_")
+    logging.info(df.head())
+    logging.info(df.columns)
 
     return df
 
-# Fonction d'harmonisation du nom des pays
-
-def harmonization_countries():
-    """ Fonction pour harmoniser le nom des pays par fichier"""
-    try :
-        data_pesticides = pd.read_csv(csv_pesticides)
-        data_rainfall = pd.read_csv(csv_rainfall)
-        data_temp = pd.read_csv(csv_temp)
-        data_yield = pd.read_csv(csv_yield)
-        mapping_countries = pd.read_csv(csv_mapping_countries)
-
-    except Exception as e:
-        logging.error(f"Erreur critique lors du chargement des fichiers : {e}")
-        raise e
-
-    # Chargement de la liste des pays pour harmonisation dans data/processed
-    mapping_df = mapping_countries
-    # On met les informations dans un dictionnaire afin de pouvoir les mapper
-    mapping_dict = dict(zip(mapping_df['Original Name'], mapping_df['Standard Name']))
-    # Identification d'une erreur supplémentaire
-    mapping_dict.update({"Bosnia And Herzegovina": "Bosnia and Herzegovina"})
-
-    # Mapping des fichiers
-    # Fichier yield
-    data_yield_map = data_yield
-    data_yield_map['Area'] = data_yield_map['Area'].map(mapping_dict).fillna(data_yield_map['Area'])
-    # Fichier rainfall
-    data_rainfall_map = data_rainfall
-    data_rainfall_map[' Area'] = data_rainfall_map[' Area'].map(mapping_dict).fillna(data_rainfall_map[' Area'])
-    # Fichier pesticide
-    data_pesticides_map = data_pesticides
-    data_pesticides_map['Area'] = data_pesticides_map['Area'].map(mapping_dict).fillna(data_pesticides_map['Area'])
-    # Fichier temp
-    data_temp_map = data_temp
-    data_temp_map['country'] = data_temp_map['country'].map(mapping_dict).fillna(data_temp_map['country'])
-
-    logging.info("Nom standardisé des pays par fichier")
-
-    return data_yield_map, data_rainfall_map, data_pesticides_map, data_temp_map
-
-def cleaning_files(data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map):
-    """"Fonction de nettoyage de chaque fichier avant fusion dans un fichier consolidé"""
-    # Fichier yield
-    # Restriction à la période commune 1990-2016
-    data_yield_map = data_yield_map[data_yield_map["Year"].between(1990, 2016)].copy()
-    # On garde les colonnes utiles
-    # On enlève notamment Domain Code / Area Code / Element Code / Item Code / Unit
-    data_yield_map = data_yield_map[["Area", "Item", "Year", "Value"]].copy()
-
-    # On renomme correctement le nom de la variable cible
-    data_yield_map = data_yield_map.rename(columns={"Area":"area",
-                                                    "Item":"item",
-                                                    "Year":"year",
-                                                    "Value":"yield"}).copy()
-    # On supprime les derniers doublons
-    data_yield_map = data_yield_map.drop_duplicates(subset=["area", "item", "year"]).copy()
-    logging.info(
-        "Doublons sur (Area, Item, Year) dans yield : %s",
-        data_yield_map.duplicated(["area", "item", "year"]).sum()
-    )
-    logging.info(
-        "Valeurs manquantes dans yield : %s",
-        data_yield_map.isnull().mean()
-)
-
-    # Fichier pesticides
-    # Restriction à la période commune 1990-2016
-    data_pesticides_map = data_pesticides_map[data_pesticides_map["Year"].between(1990, 2016)].copy()
-    # On garde les colonnes utiles
-    # On enlève notamment Domain Code / Area Code / Element Code / Item Code / Unit
-    data_pesticides_map = data_pesticides_map[["Area", "Year", "Value"]].copy()
-    # On renomme correctement le nom de la variable cible
-    data_pesticides_map = data_pesticides_map.rename(columns={"Area":"area",
-                                                            "Year":"year",
-                                                            "Value":"pesticides_tonnes"}).copy()
-    # Vérification des doublons métier
-   
-    logging.info(
-    "Doublons sur (Area, Year) dans pesticides : %s",
-    data_pesticides_map.duplicated(["area", "year"]).sum()
-    )
-    logging.info(
-        "Valeurs manquantes dans pesticides : %s",
-        data_pesticides_map.isnull().mean()
-    )
-
-    # Fichier rainfall
-    # Restriction à la période commune 1990-2016
-    data_rainfall_map = data_rainfall_map[data_rainfall_map["Year"].between(1990, 2016)].copy()
-    # On renomme correctement 2 variables
-    data_rainfall_map = data_rainfall_map.rename(columns={" Area": "area",
-                                                        "Year":"year",
-                                                        "average_rain_fall_mm_per_year": "rainfall_mm"}).copy()
-    # Conversion numérique en forçant malgrés les NaN
-    data_rainfall_map["rainfall_mm"] = pd.to_numeric(data_rainfall_map["rainfall_mm"],errors="coerce")
-    # Vérification des doublons métier
-    logging.info(
-        "Doublons sur (Area, Year) dans rainfall : %s",
-        data_rainfall_map.duplicated(["area", "year"]).sum()
-    )
-    logging.info(
-        "Valeurs manquantes dans rainfall : %s",
-        data_rainfall_map.isnull().mean()
-    )
-
-    # Fichier temp
-    # Restriction à la période commune 1990-2016
-    data_temp_map = data_temp_map[data_temp_map["year"].between(1990, 2016)].copy()
-    # On renomme correctement 2 variables
-    data_temp_map = data_temp_map.rename(columns={"country": "area"}).copy()
-    # Agrégation propre au couple Area-Year
-    # Imputation par la moyenne pour les années manquantes par pays / Imputation logique ici avant imputation globale
-    data_temp_map = (
-        data_temp_map
-        .groupby(["area", "year"], as_index=False)["avg_temp"]
-        .median()
-    ).copy()
-    # Vérification des doublons métier
-    logging.info(
-        "Doublons sur (Area, Year) dans temp : %s",
-        data_temp_map.duplicated(["area", "year"]).sum()
-    )
-    logging.info(
-        "Valeurs manquantes dans temp : %s",
-        data_temp_map.isnull().mean()
-    )
-    data_temp_map.head()
-
-    return data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map
-
-data_yield_map, data_rainfall_map, data_pesticides_map, data_temp_map = harmonization_countries()
-data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map = cleaning_files(data_yield_map, data_pesticides_map, data_rainfall_map, data_temp_map)
-logging.info(data_yield_map.head())
-logging.info(data_pesticides_map.head())
-logging.info(data_rainfall_map.head())
-logging.info(data_temp_map.head())
