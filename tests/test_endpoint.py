ae213fe175de2c0a1ad81f6c86f6bd5175818756
Florian Schorer <schorer.florian@me.com>
2026-04-03 17:13:19 +0200

    Test pour github action

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,20 +1,20 @@
 # Test des endpoints
-from fastapi.testclient import TestClient
-import pytest
-import os, sys
+#from fastapi.testclient import TestClient
+#import pytest
+#import os, sys
 
-from app import app
-import pandas as pd
+#from app import app
+#import pandas as pd
 
 # Pour tester l'application FastAPI
-client = TestClient(app)
+#client = TestClient(app)
 
 # Test endpoint/health
-def test_health_endpoint():
-    """Vérifie que le endpoint/health renvoie bien 200 et un message OK"""
-    response = client.get("/health")
-    assert response.status_code == 200, "Le endpoint /health doit répondre 200"
-    data = response.json()
-    assert "status" in data, "Le JSON doit contenir 'status'"
-    assert data["status"].lower() == "ok"
+#def test_health_endpoint():
+    #"""Vérifie que le endpoint/health renvoie bien 200 et un message OK"""
+    #response = client.get("/health")
+    #assert response.status_code == 200, "Le endpoint /health doit répondre 200"
+    #data = response.json()
+    #assert "status" in data, "Le JSON doit contenir 'status'"
+    #assert data["status"].lower() == "ok"
 
